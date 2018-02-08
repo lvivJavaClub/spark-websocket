2cb9343d679acfea25c9e98b36ca44abe6fdcadb
Andriy Levchenko <andriyl@jfrog.com>
2018-02-08 12:54:59 +0200

    Added chat history support

--- src/main/java/ChatWebSocketHandler.java
+++ src/main/java/ChatWebSocketHandler.java
@@ -1,28 +1,29 @@
-import org.eclipse.jetty.websocket.api.*;
-import org.eclipse.jetty.websocket.api.annotations.*;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
 @WebSocket
 public class ChatWebSocketHandler {
 
-    private String sender, msg;
-
     @OnWebSocketConnect
-    public void onConnect(Session user) throws Exception {
-        String username = "User" + Chat.nextUserNumber++;
-        Chat.userUsernameMap.put(user, username);
-        Chat.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
+    public void onConnect(Session user) {
+        String userID = "User" + Chat.nextUserNumber++;
+        Chat.userUsernameMap.put(user, userID);
+        Chat.sendHistory(user);
+        Chat.broadcastMessage("Server", "User " + userID + " joined the chat");
     }
 
     @OnWebSocketClose
-    public void onClose(Session user, int statusCode, String reason) {
-        String username = Chat.userUsernameMap.get(user);
-        Chat.userUsernameMap.remove(user);
-        Chat.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
+    public void onClose(Session session, int statusCode, String reason) {
+        String userId = Chat.userUsernameMap.get(session);
+        Chat.userUsernameMap.remove(session);
+        Chat.broadcastMessage("Server", userId + " left the chat");
     }
 
     @OnWebSocketMessage
-    public void onMessage(Session user, String message) {
-        Chat.broadcastMessage(sender = Chat.userUsernameMap.get(user), msg = message);
+    public void onMessage(Session session, String message) {
+        Chat.broadcastMessage(Chat.userUsernameMap.get(session), message);
     }
-
 }
